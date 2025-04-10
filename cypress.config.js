ba9c09fda37208e5d942bf776f8fbc8a5c5cc173
gabrielamonaro <gabrielamonaro@outlook.com>
2025-04-10 11:15:09 -0300

    feat: all authenticated tests needed

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://notes-serverless-app.com",
-    defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 15000,
+    requestTimeout: 15000,
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
     // },
+  },
+  chromeWebSecurity: false,
+  env: {
+    viewportWidthBreakpoint: 768,
   },
 });
